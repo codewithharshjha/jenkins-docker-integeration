1ad59dccf1b1112a79e54a9ee51577c09cbc66e1
codewithharshjha <harshjhamusicproducer@gmail.com>
2025-09-30 16:58:33 +0530

    checking jenkins is working or not

--- e-comerce-backend/config/db.js
+++ e-comerce-backend/config/db.js
@@ -5,8 +5,9 @@ const connectDB = async () => {
   try {
     console.log({MONGO_URI:process.env.MONGO_URI})
     await mongoose.connect(
-      "mongodb://hjha3987_db_user:Hjha3987%40db_user@127.0.0.1:27017/ecom?authSource=admin"|| 'mongodb://127.0.0.1/ecom',
-
+      // "mongodb://hjha3987_db_user:Hjha3987%40db_user@127.0.0.1:27017/ecom?authSource=admin"|| 'mongodb://localhost:27017/devops-ecommerce',
+'mongodb://localhost:27017/devops-ecommerce',
+// "mongodb+srv://hjha3987_db_user:Hjha3987%40@cluster0.ssyyzih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
