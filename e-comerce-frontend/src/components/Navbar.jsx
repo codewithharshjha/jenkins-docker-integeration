66a2d0c637b799dcdd4475fe463f9c7f211861ee
codewithharshjha <harshjhamusicproducer@gmail.com>
2025-10-01 12:43:08 +0530

    serving jenkins online4

--- e-comerce-frontend/src/components/Navbar.jsx
+++ e-comerce-frontend/src/components/Navbar.jsx
@@ -28,12 +28,12 @@ const Navbar = ({click}) => {
   return (
     <nav className="navbar">
       <div className="navbar__logo">
-        <h2>shashank-ecommerce</h2>
+        <h2>shashank-dtore</h2>
       </div>
 
       <ul className="navbar__links">
         <li>
-          <Link to="/cart" className="cart__link">
+          <Link to="/cart" className="cart__link ">
             <i className="fas fa-shopping-cart"></i>
             <span>
               Cart <span className="cartlogo__badge">{getCartCount()}</span>
